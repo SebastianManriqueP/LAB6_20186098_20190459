5bc29880c9ed5554475c047199345d73ecab8138
danaanv <a20202330@pucp.edu.pe>
2023-06-26 19:53:36 -0500

    dgsdfgsdfgsd

---
+++ app/src/main/java/com/example/lab06_iot/Model/ActualizarActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab06_iot.Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab06_iot.R;
+
+public class ActualizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar);
+    }
+}
