c2254ed05e4b0e574ddcdf78dce69d3bd9a807ab
danaanv <a20202330@pucp.edu.pe>
2023-06-28 12:42:44 -0500

    sdaf

--- app/src/main/java/com/example/lab06_iot/Model/ActualizarActivity.java
+++ app/src/main/java/com/example/lab06_iot/Model/ActualizarActivity.java
@@ -12,5 +12,6 @@ public class ActualizarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actualizar);
+
     }
 }
