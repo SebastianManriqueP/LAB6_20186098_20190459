f782a8b648c3cfd62143a9c3e90cb21ec0dc9538
Willy God <willy.huallpa@pucp.edu.pe>
2023-06-28 14:38:14 -0500

    trtrtr

--- app/src/main/java/com/example/lab06_iot/Model/ActualizarActivity.java
+++ app/src/main/java/com/example/lab06_iot/Model/ActualizarActivity.java
@@ -2,7 +2,9 @@ package com.example.lab06_iot.Model;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.lab06_iot.R;
 
@@ -12,6 +14,24 @@ public class ActualizarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actualizar);
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("idActividad");
+        String nombre = intent.getStringExtra("titulo");
+        String descripcion = intent.getStringExtra("descripcion");
+        String fecha = intent.getStringExtra("fecha");
+        String horini = intent.getStringExtra("horini");
+        String horfin = intent.getStringExtra("horfin");
+        EditText name = findViewById(R.id.editTextActividad);
+        EditText descrip = findViewById(R.id.editTextDescripcion);
+        EditText date = findViewById(R.id.editTextFecha);
+        EditText hori = findViewById(R.id.editTextInicio);
+        EditText horf = findViewById(R.id.editTextFinal);
+
+        name.setText(nombre);
+        descrip.setText(descripcion);
+        date.setText(fecha);
+        hori.setText(horini);
+        horf.setText(horfin);
 
     }
 }
