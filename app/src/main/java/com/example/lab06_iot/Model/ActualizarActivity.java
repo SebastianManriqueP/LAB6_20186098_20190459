c110f46c8c8446d7e19348f929b274c77e1ccadd
Sebastian Manrique <a2018609@pucp.edu.pe>
2023-06-28 23:15:25 -0500

    actulizar imagen

--- app/src/main/java/com/example/lab06_iot/Model/ActualizarActivity.java
+++ app/src/main/java/com/example/lab06_iot/Model/ActualizarActivity.java
@@ -1,12 +1,17 @@
 package com.example.lab06_iot.Model;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.lab06_iot.DatePickerFragment;
@@ -15,10 +20,17 @@ import com.example.lab06_iot.R;
 import com.example.lab06_iot.TimePickerFragment;
 import com.example.lab06_iot.TimePickerFragment2;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.protobuf.StringValue;
+
+import java.time.LocalTime;
 
 public class ActualizarActivity extends AppCompatActivity {
 
+    Uri imageUri;
+    private static final int PICK_IMAGE_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +67,14 @@ public class ActualizarActivity extends AppCompatActivity {
             mostrarTimeDialog2();
         });
 
+        Button buttonact = findViewById(R.id.buttonActualizar);
+
+        buttonact.setOnClickListener(view -> {
+            Intent intent2 = new Intent(Intent.ACTION_GET_CONTENT);
+            intent2.setType("image/*");
+            startActivityForResult(intent2, PICK_IMAGE_REQUEST);
+        });
+
         Button button = findViewById(R.id.button2);
         button.setOnClickListener(view -> {
             String nuevoNombre = name.getText().toString();
@@ -62,28 +82,54 @@ public class ActualizarActivity extends AppCompatActivity {
             String nuevaFecha = date.getText().toString();
             String nuevaHori = hori.getText().toString();
             String nuevaHorf = horf.getText().toString();
-            Actividades actualizadaActividad = new Actividades();
-            actualizadaActividad.setId(id);
-            actualizadaActividad.setDescripcion(nuevaDescripcion);
-            actualizadaActividad.setTitulo(nuevoNombre);
-            actualizadaActividad.setFecha(nuevaFecha);
-            actualizadaActividad.setInicio(nuevaHori);
-            actualizadaActividad.setFin(nuevaHorf);
-            actualizadaActividad.setImagen("img" + nuevoNombre + ".jpg");
-            FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-            firebaseFirestore.collection("usuarios")
-                    .document(usuario)
-                    .collection("actividades")
-                    .document(id)
-                    .set(actualizadaActividad).addOnSuccessListener(unused -> {
-                        Toast.makeText(this, "ACTUALIZADO", Toast.LENGTH_SHORT).show();
-                    }).addOnFailureListener(e -> e.printStackTrace());
-            Intent intentN = new Intent(this, ListaActivity.class);
-            intentN.putExtra("Usuario", usuario);
-            finishAffinity();
-            startActivity(intentN);
 
+            //Verificar tiempo de inicio menor a final
+            String[] inicioList = nuevaHori.split(":");
+            String[] finalList = nuevaHorf.split(":");
+            LocalTime tiempoInicio = LocalTime.of(Integer.parseInt(inicioList[0]), Integer.parseInt(inicioList[1]));
+            LocalTime tiempoFinal = LocalTime.of(Integer.parseInt(finalList[0]), Integer.parseInt(finalList[1]));
+            int comparacion = tiempoInicio.compareTo(tiempoFinal);
+            if (comparacion < 0) {
+                Actividades actualizadaActividad = new Actividades();
+                actualizadaActividad.setId(id);
+                actualizadaActividad.setDescripcion(nuevaDescripcion);
+                actualizadaActividad.setTitulo(nuevoNombre);
+                actualizadaActividad.setFecha(nuevaFecha);
+                actualizadaActividad.setInicio(nuevaHori);
+                actualizadaActividad.setFin(nuevaHorf);
+                actualizadaActividad.setImagen("img" + nuevoNombre + ".jpg");
+                UploadTask uploadTask = null;
+                //enviar imagen
+                if(imageUri != null) {
+                    FirebaseStorage storage = FirebaseStorage.getInstance();
+                    StorageReference storageRef = storage.getReference();
+                    StorageReference imageRef = storageRef.child("img" + nuevoNombre + ".jpg");
+                    uploadTask = imageRef.putFile(imageUri);
+                }
+                FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+                firebaseFirestore.collection("usuarios")
+                        .document(usuario)
+                        .collection("actividades")
+                        .document(id)
+                        .set(actualizadaActividad).addOnSuccessListener(unused -> {
+                            Toast.makeText(this, "ACTUALIZADO", Toast.LENGTH_SHORT).show();
+                        }).addOnFailureListener(e -> e.printStackTrace());
+                Intent intentN = new Intent(this, ListaActivity.class);
+                intentN.putExtra("Usuario", usuario);
+                if(imageUri != null) {
+                    uploadTask.addOnSuccessListener(taskSnapshot -> {
+                        finishAffinity();
+                        startActivity(intentN);
+                    });
+                }else{
+                    finishAffinity();
+                    startActivity(intentN);
+                }
+            }else{
+                Toast.makeText(this, "Tiempo de inicio debe ser menor al final", Toast.LENGTH_SHORT).show();
+            }
         });
+
 
 
     }
@@ -113,5 +159,19 @@ public class ActualizarActivity extends AppCompatActivity {
     public void mostrarTimeDialog2(){
         TimePickerFragment2 timePickerFragment2 = new TimePickerFragment2();
         timePickerFragment2.show(getSupportFragmentManager(),"timepicker2");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            // Obtener la URI de la imagen seleccionada
+            imageUri = data.getData();
+            // Establecer la imagen en el ImageView utilizando View Binding
+            ImageView imageView = findViewById(R.id.imageView5);
+            imageView.setVisibility(View.VISIBLE);
+            imageView.setImageURI(imageUri);
+        }
     }
 }
